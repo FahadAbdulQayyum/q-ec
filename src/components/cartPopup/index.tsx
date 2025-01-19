443289794098d7725127f6f72321aeca337ceda0
Fahad Abdul Qayyum <Fahad.45843@iqra.edu.pk>
2025-01-20 02:10:40 +0500

    used <></> inspite of <span></span>

--- src/components/cartPopup/index.tsx
+++ src/components/cartPopup/index.tsx
@@ -30,7 +30,7 @@ const CartPopup = () => {
     const totalPrice = cart.reduce((sum, item) => sum + item.price, 0);
 
 
-    return <span>
+    return <>
         {cart.length &&
             <div className="bg-blue-500 text-white fixed bottom-4 left-4 p-2 rounded-lg">
                 {/*/ <h2 className="text-xl font-semibold mb-4">Cart</h2> */}
@@ -49,43 +49,8 @@ const CartPopup = () => {
                 </div>
             </div>
         }
-    </span >
-
+    </>
 }
 
 export default CartPopup;
 
-
-
-// import React from 'react'
-// import { useSelector } from 'react-redux'
-// import { RootState } from '../lib/store'
-
-// const CartPopup = () => {
-//     const cart = useSelector((state: RootState) => state.cart.obj.productName);
-
-//     const productCount = cart.reduce((acc: { [key: string]: number }, product: string) => {
-//         acc[product] = (acc[product] || 0) + 1;
-//         return acc;
-//     }, {});
-
-//     const totalPrice = cart.length * 10; // Assuming each product costs $10
-
-//     return (
-//         <div className="p-4 bg-white shadow-lg rounded-lg">
-//             <h2 className="text-xl font-semibold mb-4">Cart</h2>
-//             <ul>
-//                 {Object.entries(productCount).map(([product, count]) => (
-//                     <li key={product} className="mb-2">
-//                         {product} - Quantity: {count}
-//                     </li>
-//                 ))}
-//             </ul>
-//             <div className="mt-4">
-//                 <strong>Total Price: ${totalPrice}</strong>
-//             </div>
-//         </div>
-//     )
-// }
-
-// export default CartPopup
