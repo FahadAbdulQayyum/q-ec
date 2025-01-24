12864f8034c84ca45c88e97279a3e876e6ce729b
Fahad Abdul Qayyum <Fahad.45843@iqra.edu.pk>
2025-01-24 23:16:08 +0500

    removed the commented code

--- src/app/api/signin/route.ts
+++ src/app/api/signin/route.ts
@@ -8,24 +8,3 @@ export async function POST(request: Request) {
     const body = await request.json();
     return NextResponse.json({ message: `You sent: ${body.data}` });
 }
-
-
-
-
-
-
-
-
-
-// import type { NextApiRequest, NextApiResponse } from 'next'
- 
-// type ResponseData = {
-//   message: string
-// }
- 
-// export default function handler(
-//   req: NextApiRequest,
-//   res: NextApiResponse<ResponseData>
-// ) {
-//   res.status(200).json({ message: 'Hello from Next.js!' })
-// }
