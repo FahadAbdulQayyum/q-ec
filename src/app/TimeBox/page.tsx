0f10ea95b83dd85b1fc099b41ce1b32e325b754e
Fahad Abdul Qayyum <Fahad.45843@iqra.edu.pk>
2025-01-25 22:26:33 +0500

    Commented the Calendar for a second

--- src/app/TimeBox/page.tsx
+++ src/app/TimeBox/page.tsx
@@ -1,22 +1,22 @@
 "use client"
 
-import Calendar from '@/components/Calendar'
+// import Calendar from '@/components/Calendar'
 import React from 'react'
-import moment from 'moment-timezone';
+// import moment from 'moment-timezone';
 
 
-const handleSelect = (unixDate: number) => {
-    console.log("Selected date:", unixDate);
-};
+// const handleSelect = (unixDate: number) => {
+//     console.log("Selected date:", unixDate);
+// };
 
 const TimeBox = () => {
     return (
         <div>
-            <Calendar
+            {/* <Calendar
                 timeZoneName="America/New_York"  // Set your desired time zone
                 selected={moment().unix()}        // Provide the selected date in Unix format
                 onSelect={handleSelect}             // Provide the select function to handle date selection
-            />
+            /> */}
         </div>
     )
 }
