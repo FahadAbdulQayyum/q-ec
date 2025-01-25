3478558b7e1598c36226042535f51278ed442744
Fahad Abdul Qayyum <Fahad.45843@iqra.edu.pk>
2025-01-25 22:30:22 +0500

    Commented the Week and DayNames for a second too

--- src/components/Calendar/DayNames.tsx
+++ src/components/Calendar/DayNames.tsx
@@ -1,18 +1,18 @@
-// DayNames.tsx
-import { Typography } from '@shadcn/ui';
+// // DayNames.tsx
+// import { Typography } from '@shadcn/ui';
 
-const DayNames = () => {
-    const days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+// const DayNames = () => {
+//     const days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
 
-    return (
-        <div className="flex justify-between px-4 py-2">
-            {days.map((day) => (
-                <Typography key={day} variant="body2" className="flex-1 text-center">
-                    {day}
-                </Typography>
-            ))}
-        </div>
-    );
-};
+//     return (
+//         <div className="flex justify-between px-4 py-2">
+//             {days.map((day) => (
+//                 <Typography key={day} variant="body2" className="flex-1 text-center">
+//                     {day}
+//                 </Typography>
+//             ))}
+//         </div>
+//     );
+// };
 
-export default DayNames;
+// export default DayNames;
