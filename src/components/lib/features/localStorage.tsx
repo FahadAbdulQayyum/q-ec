0fef9855a8e559a1e36344fe5dc62314358d1999
Fahad Abdul Qayyum <Fahad.45843@iqra.edu.pk>
2025-01-27 21:01:05 +0500

    Loader size decrease for better UI/UX experience and defined localstorage

--- src/components/lib/features/localStorage.tsx
+++ src/components/lib/features/localStorage.tsx
@@ -1,3 +1,4 @@
+"use client"
 // import { UserInfo, UserInfoState } from './userInfo/userInfoSlice';
 import { UserInfoState } from './userInfo/userInfoSlice';
 import { CartState } from './cart/cartSlice';
