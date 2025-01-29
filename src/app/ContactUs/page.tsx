e2951efe1070a27211d2570497dd93d3eab241af
Fahad Abdul Qayyum <Fahad.45843@iqra.edu.pk>
2025-01-30 00:58:01 +0500

    ContactUs page edited to get the testing passed by adding attributes

--- src/app/ContactUs/page.tsx
+++ src/app/ContactUs/page.tsx
@@ -3,8 +3,9 @@ import React from "react"
 
 const ContactUs = () => {
   return (
-    <div>
-      <ContactUsComponent/>
+    <div data-testid="contact-us-page">
+      <h1 data-testid="contact-us-heading">Contact Uss</h1>
+      <ContactUsComponent />
     </div>
   )
 }
