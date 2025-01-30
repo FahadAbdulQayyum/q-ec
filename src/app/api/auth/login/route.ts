e9800c17343921f974e510877398108dd1a2b085
Fahad Abdul Qayyum <Fahad.45843@iqra.edu.pk>
2025-01-30 21:28:37 +0500

    added login/route.ts file

---
+++ src/app/api/auth/login/route.ts
@@ -0,0 +1,45 @@
+import { NextRequest, NextResponse } from "next/server";
+import { cookies } from "next/headers";
+
+export async function POST(req: NextRequest) {
+  try {
+    const { email, password } = await req.json();
+
+    console.log("email >>", email);
+    console.log("password >>", password);
+
+    // Temporary Hardcoded credentials
+    const validEmail = "fahad@gmail.com";
+    const validPassword = "fahad";
+
+    if (email === validEmail && password === validPassword) {
+      // Set HttpOnly cookie
+      cookies().set({
+        name: "token",
+        value: "true",
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        path: "/",
+        maxAge: 60 * 60 * 24, // 1 day expiration
+      });
+
+      return NextResponse.json(
+        { message: "Login successful" },
+        { status: 200 }
+      );
+    }
+
+    return NextResponse.json(
+      { message: "Invalid credentials" },
+      { status: 401 }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      { message: "Error processing request" },
+      { status: 500 }
+    );
+  }
+}
+
+// stateful authentication - Bank methodology
+// Stateless authentication - 
